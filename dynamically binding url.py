2ed94dccdf5add80ae1b1df95f625b09154be114
ishan-nitj <ishan.nitj@gmail.com>
2016-05-23 01:09:45 +0530

    changes made

---
+++ dynamically binding url.py
@@ -0,0 +1,24 @@
+from flask import Flask, redirect, url_for
+app = Flask(__name__)
+
+@app.route('/admin')
+def hello_admin():
+   return 'Hello Admin'
+
+@app.route('/guest/<guest>')
+def hello_guest(guest):
+   return 'Hello %s as Guest' % guest
+
+@app.route('/user/<name>')
+def hello_user(name):
+   if name =='admin':
+      return redirect(url_for('hello_admin'))
+   else:
+      return redirect(url_for('hello_guest',guest = name))
+
+if __name__ == '__main__':
+   app.run(debug = True)
+
+#The User() function checks if an argument received matches ‘admin’ or not. If it matches, the application is redirected to the hello_admin() function using
+#url_for(), otherwise to the hello_guest() function passing the received argument as guest parameter to it.
+
